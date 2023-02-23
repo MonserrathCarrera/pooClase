18d17eae176cbf2b20199662a44f2c105e95e914
MonserrathCarrera <2022-118671@intecap.edu.gt>
2023-02-22 22:27:50 -0600

    agregué código

--- src/pooclase/PooClase.java
+++ src/pooclase/PooClase.java
@@ -1,14 +1,14 @@
-
 package pooclase;
 
 // POO  EJERCICIO 1 CUENTAS BANCARIAS
-public class PooClase {
-int  marca;
-  int precio=0;
-  int modelo;
+public class PooClase {                                                 //CLASE PRIVADA
+
+    int marca;                                                                   // ATRIVUTOS 
+    int precio = 0;
+    int modelo;
     private int doble;
 
-  /*  public PooClase(int saldo) {
+    /*  public PooClase(int saldo) {                                    // EJERCICIO DE CLASAE MARTES
         this.saldo = saldo;
     }
     
@@ -18,48 +18,91 @@ public void CuentasAbonar (int saldo){
 public void CuentasDebito (int saldo){
     this.saldo -= saldo;
     }*/
-public void ProductoElectronico (){
-    getPrecioElectronico();
-    getMarcaElectronico();
-    getModeloElectronico();
-   setPrecioElectronico(double PrecioElectronico);
-    setPrecioElectronico(Srtring  MarcaElectronico);
-    setPrecioElectronico(Srtring  ModeloElectronico);
-}
+    public void ProductoElectronico() {// CLASE PRODUCTO ELECTRONICO
+        getPrecioElectronico();// MÉTODOS GET
+        getMarcaElectronico();
+        getModeloElectronico();
+        setPrecioElectronico(double PrecioElectronic);// MÉTODOS SET 
+        setPrecioElectronico(Srtring  MarcaElectronico );
+        setPrecioElectronico(Srtring  ModeloElectronico);
 
-    private int getPrecioElectronico (){
-        System.out.println("el precio del dispositivo es:"+ precio);
-            return precio;
+      }
+    
+    private int getPrecioElectronico() {  // MÉTODOS GET
+        System.out.println("el precio del dispositivo es:" + precio);
+        return precio;
     }
-    
-    
-    private int getMarcaElectronico(){
-      System.out.println("La marca del dispositivo es:"+marca);
+
+    private int getMarcaElectronico() {  // MÉTODOS GET
+        System.out.println("La marca del dispositivo es:" + marca);
         return marca;
     }
 
-    
-    private int getModeloElectronico(){
-      System.out.println("El modelo del dispositivo es:"+modelo);
-              return modelo;
+    private int getModeloElectronico() {  // MÉTODOS GET
+        System.out.println("El modelo del dispositivo es:" + modelo);
+        return modelo;
 
     }
 
-    public void setPrecioElectronico(double precio) {
-      this.precio=(int) precio;
-    }
-     public void setMarcaElectronico(double marca) {
-      this.precio=(int) precio;
-    }
-      public void setModeloElectronico(double modelo) {
-      this.precio=(int) precio;
+    public void setPrecioElectronico(double PrecioElectronico) {   // MÉTODOS SET
+        this.precio = (int) precio;
     }
 
-    
+    public void setMarcaElectronico(String MarcaElectronico) {   // MÉTODOS SET
+        this.marca = (int) precio;
     }
 
+    public void setModeloElectronico(String ModeloElectronico) {   // MÉTODOS SET
+        this.modelo = (int) precio;
+    }
+
+    public class TelefonoMovil {  // CLASE TELEFONO MOVIL 
+
+        int CapacidadAlmacenamiento; // ATRIVUTOS 
+        int DuracionBateria;
+
+        private int getCapacidadAlmacenamiento() { // MÉTODOS GET
+            System.out.println("Almacenmiento:" + CapacidadAlmacenamiento);
+            return CapacidadAlmacenamiento;
+        }
+
+        private int getDuracionBateria() { // MÉTODOS GET
+            System.out.println("Almacenmiento:" + DuracionBateria);
+            return DuracionBateria;
+        }
+
+        public void setCapacidadAlmacenamiento(int CapacidadAlmacenamiento) { // MÉTODOS SET
+            this.CapacidadAlmacenamiento = (int) CapacidadAlmacenamiento;
+        }
+
+        public void setDuracionBateria(int DuracionBateria) { // MÉTODOS SET
+            this.DuracionBateria = (int) DuracionBateria;
+        }
+    }
     
     
+  public class Tableta {  // CLASE TABLETA
+    
+    Double TamanoPantlla;
+    String ResolucionPantalla;
     
     
+    private Double getTamanoPantlla() { // MÉTODOS GET
+            System.out.println("Almacenmiento:" + TamanoPantlla);
+            return TamanoPantlla;
+        }
+
+        private String getResolucionPantalla() { // MÉTODOS GET
+            System.out.println("Almacenmiento:" + ResolucionPantalla);
+            return ResolucionPantalla;
+        }
+
+        public void setTamanoPantlla(int TamanoPantalla) { // MÉTODOS SET
+            this.TamanoPantalla;
+        }
+
+        public void setResolucionPantalla(int ResolucionPantalla{ // MÉTODOS SET
+            this.ResolucionPantalla;
+        }
+}
 }
